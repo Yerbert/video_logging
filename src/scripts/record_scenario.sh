901f8ed499178a3e9f87c9477f5a3e4b205d2eea
KuYewSiang <105586484+KuYewSiang@users.noreply.github.com>
2023-01-16 19:41:32 +1100

    Updated record_scenario.py and process_messages.py

---
+++ src/scripts/record_scenario.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+camera="camera/color/image_raw/compressed/processed"
+point_cloud="velodyne_points/processed"
+info_logs="infologs"
+transform="tf/processed"
+
+# name=Scene_$(date +'%k-%M-%S_%d-%m-%Y').bag
+name=SceneRecording
+
+rosbag record -O $name $camera $point_cloud $info_logs $transform __name:=record_node
+
+echo -e "\nRECORDING STOPPED"
+
+rosnode kill /record_node
+
+rosbag info $name
