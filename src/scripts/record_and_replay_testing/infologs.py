b9dbcb949c505d4c87290933c7382bf9c86c7955
KuYewSiang <105586484+KuYewSiang@users.noreply.github.com>
2023-01-24 19:09:33 +1100

    Modified run_scene.py and infologs.py
    Modified run_scene.py to include pause/play function. 
    Modified infologs.py to make sure the loops syncs at correct timings.

--- src/scripts/record_and_replay_testing/infologs.py
+++ src/scripts/record_and_replay_testing/infologs.py
@@ -3,16 +3,17 @@ from std_msgs.msg import String
 
 def infologs():
     pub = rp.Publisher('/infologs', String, queue_size=10)
-    rp.sleep(0.5)
-    pub.publish(String("1. Heading to Point B..."))
-    rp.sleep(14.5)
-    pub.publish(String("2. done"))
-    rp.sleep(1)
-    pub.publish(String("3. Exiting arena..."))
-    rp.sleep(9)
-    pub.publish(String("4. ERROR [Navigation] - No suitable path to destination"))
-    rp.spin()
-
+    while(True):
+        rp.sleep(0.5)
+        pub.publish(String("1. Heading to Point B..."))
+        rp.sleep(14.5)
+        pub.publish(String("2. done"))
+        rp.sleep(1)
+        pub.publish(String("3. Exiting arena..."))
+        rp.sleep(9)
+        pub.publish(String("4. ERROR [Navigation] - No suitable path to destination"))
+        rp.sleep(2.332065)
+        
 if __name__ == "__main__":
     rp.init_node("infologger", anonymous=True)
     infologs()
--- src/scripts/record_and_replay_testing/run_scene.py
+++ src/scripts/record_and_replay_testing/run_scene.py
@@ -4,6 +4,7 @@ import rospy
 import subprocess
 import os
 import signal
+import psutil
 import sys
 import argparse
 
@@ -18,13 +19,34 @@ from std_msgs.msg import Int32, Float32, String, Bool
 from tf2_msgs.msg import TFMessage
 from time import sleep
 
+# Global Variables
 p1 = None
 p2 = None
+p2Process = None
+pause = False
+service = ""
 
+# Signal Handler to kill all processes
 def signal_handler(sig, frame):
     os.killpg(os.getpgid(p1.pid), signal.SIGTERM)
     os.killpg(os.getpgid(p2.pid), signal.SIGTERM) 
     sys.exit(0)
+
+# Pause flipflop mechanism
+def pause_callback(data):
+    global pause,service,p2Process
+    pause = not pause
+    if pause:
+        rospy.loginfo("Paused")
+        command = "rosservice call " + service + " true"
+        os.system(command)
+        p2Process.suspend()
+    else:
+        rospy.loginfo("Resumed")
+        command = "rosservice call " + service + " false"
+        os.system(command)
+        p2Process.resume()
+
 
 if __name__ == '__main__':
     
@@ -43,8 +65,9 @@ if __name__ == '__main__':
     #Initialise ROS node to enable communication with other nodes
     rospy.init_node('rosbag_replay')
 
-    #Initialize Rostopics publisher for /tf offset
+    #Initialize Rostopics publisher and subscribers
     tf_offset_pub = rospy.Publisher("/tf/offset", TFMessage, queue_size=10)
+    pause_sub = rospy.Subscriber("/pause", Bool, pause_callback)
 
     # Find the last /tf rostopic and publish it
     tf_offset_msg = None
@@ -59,15 +82,30 @@ if __name__ == '__main__':
     # Start subprocess to play rosbag
     command = "rosbag play -l " + args.rosbag
     p1 = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True, cwd=cwd_path, executable='/bin/bash', preexec_fn=os.setsid)
+    sleep(0.5)
+
+    # Find the rosservice for pausing/playing rosbag
+    list_cmd = subprocess.Popen("rosservice list", shell=True, stdout=subprocess.PIPE)
+    list_output = list_cmd.stdout.read()
+    retcode = list_cmd.wait()
+    assert retcode == 0, "List command returned %d" % retcode
+    for string in list_output.decode('ascii').split("\n"):
+        if (string.endswith('pause_playback')):
+            service = string
+            print("\nPausing service: " + service)
 
     # Start the infologs file
-    command = "python " + args.infologs
+    command = "python3 " + args.infologs
     p2 = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True, cwd=cwd_path, executable='/bin/bash', preexec_fn=os.setsid)
+    p2Process = psutil.Process(pid=p2.pid)
 
-    # Infinite loop to keep parent process running
+    # Infinite loop to keep parent process running and listen in to pause/play functionality
     while True:
-        print("Running...")
-        sleep(2.0)
+
+        while(pause):
+            sleep(0.5)
+
+        sleep(0.5)
     
 
     
