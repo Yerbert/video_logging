185783a0076fa1cedc7398281b6fa5a147b19c79
Lachlanric <lachlanric@outlook.com>
2023-01-31 16:38:40 +1100

    Rosbag now has /start signal

--- src/scripts/record_and_replay_testing/insert_messages_into_rosbag.py
+++ src/scripts/record_and_replay_testing/insert_messages_into_rosbag.py
@@ -1,7 +1,7 @@
 from rospy import rostime
 import rosbag
 from collections import namedtuple
-from std_msgs.msg import String
+from std_msgs.msg import String, Bool
 import sys
 import shutil
 
@@ -14,18 +14,24 @@ def insert_infologs(bag_name, messages):
         bag.write("/infologs", String(infolog.message), rostime.Time.from_seconds(infolog.time + start_time))
     bag.close()
 
+def insert_start_message(bag_name):
+    bag = rosbag.Bag(bag_name, 'a')
+    start_time = bag.get_start_time()
+    bag.write("/start", Bool(True), rostime.Time.from_seconds(start_time))
+    bag.close()
 
 if __name__ == "__main__":
 
     messages = [
-        InfoLog(0.5, "1. Heading to Point B..."),
-        InfoLog(15,  "2. done"),
-        InfoLog(16,  "3. Exiting arena..."),
-        InfoLog(25,  "4. ERROR [Navigation] - No suitable path to destination")
+        InfoLog(1,  "1) Payload on board: YES"),
+        InfoLog(5,  "2) Payload on board: YES"),
+        InfoLog(14, "3) Arriving at destination..."),
+        InfoLog(17, "4) Payload on board: NO")
     ]
 
     bag_name = sys.argv[1]
     new_bag_name = bag_name[:-4] + "_with_infologs.bag"
     shutil.copyfile(bag_name, new_bag_name)
     insert_infologs(new_bag_name, messages)
+    insert_start_message(new_bag_name)
 
