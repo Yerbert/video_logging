7b8539a73552866fc1b739f8d514bc1e20ac12cc
KuYewSiang <ykuu0003@student.monash.edu>
2023-02-03 15:15:46 +1100

    Edit rosbag_player to include option to launch tcp

--- src/scripts/AR22_Scripts/rosbag_player.py
+++ src/scripts/AR22_Scripts/rosbag_player.py
@@ -1,9 +1,18 @@
 import rospy as rp
 import rosbag
+import rostopic
+import rosnode
 import pyrosbag
+import subprocess
+import os
 import sys
+import signal
 from std_msgs.msg import Bool
 from tf2_msgs.msg import TFMessage
+from time import sleep
+
+#Global
+tcp = False
 
 class PauseSubscriber():
     def __init__(self, rosbag_process):
@@ -30,10 +39,41 @@ def publish_tfs(rosbag_name):
             if (tf.child_frame_id == "base_link" and tf.header.frame_id == "odom") or (tf.child_frame_id == "odom" and tf.header.frame_id == "map"):
                 tf_path.append(tf)
     tf_pub.publish(TFMessage(tf_path))
-    
+
+def signal_handler(sig, frame):
+    if (tcp):
+        os.killpg(os.getpgid(p.pid), signal.SIGTERM) 
+    sys.exit(0)
 
 if __name__ == "__main__":
 
+
+    # Get rosbag name and tcp option
+    rosbag_name = sys.argv[1]
+    if len(sys.argv) == 3:
+        tcp = sys.argv[2]
+
+    # Signal Handler to kill subprocesses
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Start subprocess to play start tcp
+    if (tcp):
+        command = "roslaunch ros_tcp_endpoint endpoint.launch"
+        p = subprocess.Popen(command, stdin=subprocess.PIPE, shell=True, cwd=os.getcwd(), executable='/bin/bash', preexec_fn=os.setsid)
+        sleep(2.0)
+
+    # Check if tcp endpoint is initialized
+    try:
+        rostopic.get_topic_class('/rosout')
+        rosnode_list = rosnode.get_node_names()
+        if '/unity_endpoint' not in rosnode_list:
+            print("[ERROR] Please start a tcp endpoint before playing rosbag.")
+            sys.exit(0)
+    except:
+        print("[ERROR] Roscore and tcp endpoint must be active before playing rosbag!")
+        sys.exit(0)
+    
+    # Initialize node
     rp.init_node("rosbag_replay")
 
     # Get rosbag name
