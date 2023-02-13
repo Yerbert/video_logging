9b0b3b98203553db080b715e7ee83c898c63f0e6
Lachlanric <lachlanric@outlook.com>
2023-02-13 21:58:18 +1100

    Expanded Jackal wrapper

---
+++ src/scripts/AR22_Scripts/JackalSSH.py
@@ -0,0 +1,43 @@
+import subprocess
+import signal
+import rospy
+import os
+import json
+from video_logging.msg import FilterSwitch
+
+class JackalSSH:
+    def __init__(self):
+        self.process = subprocess.Popen(
+            "sshpass -p clearpath ssh -tt administrator@160.69.69.10\n",
+            shell=True,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            preexec_fn=os.setsid
+        )
+    
+    def send_cmd(self, cmd):
+        self.process.stdin.write((cmd+"\n").encode())
+        self.process.stdin.flush()
+        return self
+
+    def ros_pub(self, topic_name, msg_type, data):
+        self.send_cmd("rostopic pub -1 {} {} {}".format(
+            topic_name,
+            msg_type,
+            json.dumps(data)
+        ))
+        return self
+    
+    def ros_pub_filterswitch(self, msg):
+        # msg must be a FilterSwitch
+        msg_data_text = ('{' + str(msg).replace('\n',', ') + '}').lower()
+        self.ros_pub("/filters", "video_logging/FilterSwitch", msg_data_text)
+        return self
+
+    def ros_pub_condition(self, condition):
+        self.ros_pub("/condition", "std_msgs/String", condition)
+        return self
+    
+    def kill(self):
+        os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
+        del self.process
